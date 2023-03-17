7380cc81da080a47151566ad876421d048df5e86
elmouy <elmoutaouakkaly@gmail.com>
2023-03-17 15:50:48 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
-		{
-                    if (i != 4 && i != 16)
-		    {
-		     putchar(alp[i]);
-		    }
-	}
-	putchar('\n');
-	return (0);
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+if (i != 4 && i != 16)
+{
+putchar(alp[i]);
+}
+}
+putchar('\n');
+return (0);
 }
