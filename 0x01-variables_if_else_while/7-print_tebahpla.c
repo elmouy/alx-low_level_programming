c7ada778f754593017dadca257278cc01224f3e2
elmouy <elmoutaouakkaly@gmail.com>
2023-03-17 16:54:58 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
 
-	for (i = 25; i > 0; i--)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
