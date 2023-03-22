48adcc44b6603250496a2676fea088662aa4c38b
elmouy <elmoutaouakkaly@gmail.com>
2023-03-21 17:39:58 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,3 @@
-/*
- * File: 101-natural.c
- * Auth: vincent kip
- */
-
 #include <stdio.h>
 
 /**
@@ -13,17 +8,17 @@
  */
 int main(void)
 {
-	int i, s= 0;
+	int i;
+	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
-			s += i;
+			sum += i;
 	}
 
-	_putchar('s');
+	_putchar('sum');
 	_putchar('\n');
 
 	return (0);
 }
-
