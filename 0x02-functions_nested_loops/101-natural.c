b4c5d9de426184c772a62e96d0333fac590630ef
elmouy <elmoutaouakkaly@gmail.com>
2023-03-21 17:51:56 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,25 +8,15 @@
  */
 int main(void)
 {
-        int i, sum = 0;
-        char str[12];
-        int len;
+	int i, sum = 0;
 
-        for (i = 0; i < 1024; i++)
-        {
-                if ((i % 3) == 0 || (i % 5) == 0)
-                        sum += i;
-        }
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
 
-        sprintf(str, "%d", sum);  // Convert integer sum to character string
-        len = strlen(str);
+	printf("%d\n", sum);
 
-        for (i = 0; i < len; i++)
-        {
-                putchar(str[i]);
-        }
-
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
