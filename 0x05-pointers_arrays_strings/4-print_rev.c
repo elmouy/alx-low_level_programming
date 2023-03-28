db4baa1a95f5466a352470f6a8f718eb4c8f0cd7
elmouy <elmoutaouakkaly@gmail.com>
2023-03-27 17:15:49 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * print_rev, prints a string, in reverse, followed by a new line
- * @s: string to print
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
  */
 void print_rev(char *s)
 {
