7e6bea20fdccbaa897dc53645b153cd5710446ed
elmouy <elmoutaouakkaly@gmail.com>
2023-03-27 18:20:22 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,22 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	for (i = 0 ; str[i] != '\0'; i++)
+	while (*y != '\0')
 	{
-		if (str[i] % 2 == 0)
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
 	{
-		_putchar(str[i]);
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
