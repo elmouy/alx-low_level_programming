89f04de22c27a20896c271cab0d05fea939afd0a
elmouy <elmoutaouakkaly@gmail.com>
2023-04-11 05:44:09 -0700

    malloc free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, j= 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	p = malloc(sizeof(char) * (i + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (j = 0; str[j]; j++)
+		p[j] = str[j];
+
+	return (j);
+}
+
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	conct = malloc(sizeof(char) * (i + j + 1));
+
+	if (p == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		p[i] = s2[j];
+		i++, j++;
+	}
+	p[i] = '\0';
+	return (p);
+}
