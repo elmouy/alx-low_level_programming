d54a123915e5bec38b00c7325e52ca54e249c6cd
elmouy <elmoutaouakkaly@gmail.com>
2023-04-27 15:37:47 -0700

    all

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,62 +1,31 @@
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - function with two arguments
- * @head: double pointer to linked list
- * @str: string pointer
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Description: adds a new node at the beginning
- * Return: address of new element
+ * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int count = 0;
-	list_t *temp;
+	list_t *ptr;
+	unsigned int i = 0;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
-	temp->str = _strdup(str);
-	while (str[count] != '\0')
-		count++;
-	temp->len = count;
-	temp->next = *head;
-	*head = temp;
-	return (temp);
-}
-
-/**
- * *_strdup - function with one argument
- * @str: string argument
- *
- * Description: returns a pointer to allocated space in memory
- * Return: pointer
- */
-char *_strdup(const char *str)
-{
-	int i, j;
-	char *ptr;
-
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (*(str + i) != '\0')
-	{
+	while (str[i])
 		i++;
-	}
 
-	ptr = malloc(sizeof(char) * i + 1);
-
-	if (ptr == NULL)
+	new = malloc(sizeof(list_t));
+	if (!ptr)
 		return (NULL);
 
-	j = 0;
-	while (str[j] != '\0')
-	{
-		ptr[j] = str[j];
-		j++;
-	}
-	ptr[j] = '\0';
-	return (ptr);
+	ptr->str = strdup(str);
+	ptr->i = i;
+	ptr->next = (*head);
+	(*head) = ptr;
+
+	return (*head);
 }
+
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,62 +1,41 @@
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - function with two arguments
- * @head: double pointer to linked list
- * @str: string pointer
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
  *
- * Description: adds a new node at the beginning
- * Return: address of new element
+ * Return: address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	int count = 0;
-	list_t *temp;
+	list_t *p;
+	list_t *e = *head;
+	unsigned int i = 0;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
-	temp->str = _strdup(str);
-	while (str[count] != '\0')
-		count++;
-	temp->len = count;
-	temp->next = *head;
-	*head = temp;
-	return (temp);
-}
-
-/**
- * *_strdup - function with one argument
- * @str: string argument
- *
- * Description: returns a pointer to allocated space in memory
- * Return: pointer
- */
-char *_strdup(const char *str)
-{
-	int i, j;
-	char *ptr;
-
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (*(str + i) != '\0')
-	{
+	while (str[i])
 		i++;
-	}
 
-	ptr = malloc(sizeof(char) * i + 1);
-
-	if (ptr == NULL)
+	p = malloc(sizeof(list_t));
+	if (!p)
 		return (NULL);
 
-	j = 0;
-	while (str[j] != '\0')
+	p->str = strdup(str);
+	p->i = i;
+	p->next = NULL;
+
+	if (*head == NULL)
 	{
-		ptr[j] = str[j];
-		j++;
+		*head = p;
+		return (p);
 	}
-	ptr[j] = '\0';
-	return (ptr);
+
+	while (e->next)
+		e = e->next;
+
+	e->next = p;
+
+	return (p);
 }
