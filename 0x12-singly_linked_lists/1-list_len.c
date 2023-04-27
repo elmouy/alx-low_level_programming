32b0ae4b28ff70f29fa756071b5a202180465fc8
elmouy <elmoutaouakkaly@gmail.com>
2023-04-27 12:04:14 -0700

    all

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function with one argument
+ * @h: const list_t pointer argument to struct
+ *
+ * Description: returns the number of elements in a linked list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i= 0;
+
+	if (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
