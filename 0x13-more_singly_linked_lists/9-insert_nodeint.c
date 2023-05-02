ca40b5c7735a88f6230bd8670d277baf137c4980
elmouy <elmoutaouakkaly@gmail.com>
2023-05-02 16:02:03 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index -  inserts a new node at a given position
+* insert_nodeint_at_index - inserts a new node at a given position
 * @head: pointer
 * @idx: index
 * @n: integer
