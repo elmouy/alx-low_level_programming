75b9b6fa9be43478c69994b625ed62ff58e26591
elmouy <elmoutaouakkaly@gmail.com>
2023-05-02 16:05:02 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,11 +27,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = node;
 		return (node);
 	}
-	while ((tmp != NULL) && (i < idx))
+	for (i = 0; (tmp != NULL) && (i < idx); i++)
 	{
 		if (i == idx - 1)
 		{
-			i++;
 			node->next = tmp->next;
 			tmp->next = node;
 			return (node);
