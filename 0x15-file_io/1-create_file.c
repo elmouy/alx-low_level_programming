232e3ac30c76b887586c02a1857e451978b719b2
elmouy <elmoutaouakkaly@gmail.com>
2023-05-08 15:26:39 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,53 +1,26 @@
 #include "main.h"
 
-int _strlen(char *str);
-
 /**
- * create_file - a function to create a file and write
- * @filename: the file to be created
- * @text_content: a null byte terminated string
- * to be written into our new file.
- * Return: 1 on success, -1 on failure for any reason.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int f, w, c;
-	int l;
-
 	if (filename == NULL)
 		return (-1);
 
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (f == 0)
-		return (-1);
+	FILE *fp = fopen(filename, "w");
 
+	if (fp == NULL)
+		return (-1);
+	
 	if (text_content != NULL)
-		l = _strlen(text_content);
-	else
-		return (1);
-
-	w = write(f, text_content, l);
-	if (w < 0)
-		return (-1);
-
-	c = close(f);
-	if (c < 0)
-		return (-1);
+		fwrite(text_content, strlen(text_content), 1, fp);
+	fclose(fp);
 	return (1);
-}
-
-/**
- * _strlen - a function to return the length of a string
- * @str: a char pointer
- * Return: s
- */
-
-int _strlen(char *str)
-{
-	int l;
-
-	for (l = 0; s[l] != '\0'; l++)
-		continue;
-	return (s);
 }
