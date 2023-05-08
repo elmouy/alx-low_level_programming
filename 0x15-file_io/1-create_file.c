343619a7ef154929d892739655f05138caf92033
elmouy <elmoutaouakkaly@gmail.com>
2023-05-08 15:46:00 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,27 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int f, w, i = 0;
+
 	if (filename == NULL)
 		return (-1);
 
-	int f = open(filename, O_WRONLY | O_APPEND);
-
-	if (f == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
-		int i = 0;
-
-		while (text_content[i])
+		for (i = 0; text_content[i];)
 			i++;
-		int r = write(f, text_content, i);
-
-		if (r == -1)
-		{
-			close(f);
-			return (-1);
-		}
 	}
+
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(f, text_content, i);
+
+	if (f == -1 || w == -1)
+		return (-1);
+
 	close(f);
+
 	return (1);
 }
