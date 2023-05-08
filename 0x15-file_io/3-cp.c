72f736fb02562b89f7650bb3874fe0df810ce77e
elmouy <elmoutaouakkaly@gmail.com>
2023-05-08 16:26:21 -0700

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,65 +23,65 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	if (argv[1] == NULL)
-		cant_read(argv[1]);
+		read(argv[1]);
 
 	f1 = open(argv[1], O_RDONLY);
 	if (f1 == -1)
-		cant_read(argv[1]);
+		f_read(argv[1]);
 
 	f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f2 == -1)
-		cant_write(argv[2]);
+		f_write(argv[2]);
 
 	r = read(fd1, b, 1024);
 	if (r == -1)
-		cant_read(argv[1]);
+		f_read(argv[1]);
 	w = write(f2, bf, r);
 	if (w == -1)
-		cant_write(argv[2]);
+		f_write(argv[2]);
 
 	while (r == 1024)
 	{
 		r = read(f1, b, 1024);
 		if (r == -1)
-			cant_read(argv[1]);
+			f_read(argv[1]);
 
 		w = write(f2, b, r);
 		if (w == -1)
-			cant_write(argv[2]);
+			f_write(argv[2]);
 	}
 
 	if (close(f1) < 0)
-		cant_close(f1);
+		f_close(f1);
 	if (close(f2) < 0)
-		cant_close(f2);
+		f_close(f2);
 
 	return (0);
 }
 /**
- * read - a function to print an error and exit
+ * f_read - a function to print an error and exit
  * @f_frm: a pointer to our originalfile
  */
-void read(char *f_frm)
+void f_read(char *f_frm)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 	exit(98);
 }
 /**
- * write - a function to print an error message and exit
+ * f_write - a function to print an error message and exit
  * @f_to: a pointer to our receiving file
  */
-void write(char *f_to)
+void f_write(char *f_to)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 	exit(99);
 }
 
 /**
- * close - a function to print out an error and exit
+ * f_close - a function to print out an error and exit
  * @f: our file descriptor
  */
-void close(int f)
+void f_close(int f)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 	exit(100);
