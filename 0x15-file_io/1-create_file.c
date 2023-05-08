ca084c47051cae5a4781db7e564676c1544c7b27
elmouy <elmoutaouakkaly@gmail.com>
2023-05-08 15:31:59 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,58 @@
 #include "main.h"
 
+int _strlen(char *s);
+
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - a function to create a file and write
+ * some text into it.
+ * @filename: the file to be created
+ * @text_content: a null byte terminated string
+ * to be written into our new file.
+ * Return: 1 on success, -1 on failure for any reason.
  */
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd, wrt, cls;
+	int len;
+
 	if (filename == NULL)
 		return (-1);
 
-	FILE *fp = fopen(filename, "w");
-
-	if (fp == NULL)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
-		fwrite(text_content, strlen(text_content), 1, fp);
-	fclose(fp);
+		len = _strlen(text_content);
+	else
+		return (1);
+
+	wrt = write(fd, text_content, len);
+	if (wrt < 0)
+		return (-1);
+
+	cls = close(fd);
+	if (cls < 0)
+		return (-1);
 	return (1);
+}
+
+/**
+ * _strlen - a function to return the length of a string
+ *
+ * @s: a char pointer passed from main
+ *
+ * Return: returns an int for the length of s
+ */
+
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (s);
 }
